22b43d390ee97ab4174a8fcf71c2fa13058ce7a7
lewe189 <lewe@mplan.site>
2025-01-17 15:43:38 +0800

    一种简单的记分板实现

--- src/main/java/com/BeRusted/ReactionElement/events/FireEvent.java
+++ src/main/java/com/BeRusted/ReactionElement/events/FireEvent.java
@@ -2,21 +2,46 @@ package com.BeRusted.ReactionElement.events;
 
 import com.BeRusted.ReactionElement.registers.ItemsRegister;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.text.TextComponentString;
-import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.scoreboard.Score;
+import net.minecraft.scoreboard.ScoreObjective;
+import net.minecraft.scoreboard.Scoreboard;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class FireEvent {
     @SubscribeEvent
-    public void FireEvent(AttackEntityEvent event){
-        EntityPlayer player = event.getEntityPlayer();
-        Entity target = event.getTarget();
-        if(!event.getEntityPlayer().world.isRemote){
-            if (player.getHeldItemMainhand().getItem().getRegistryName() == ItemsRegister.flame_sword.getRegistryName()){
-                player.sendMessage(new TextComponentString(player.getName()+" attacked "+target.getName()));
+    public void onEntityAttacked(LivingHurtEvent event) {
+        // 检测实体攻击
+        if (event.getSource().getTrueSource() instanceof EntityLivingBase) {
+            EntityLivingBase attacker = (EntityLivingBase) event.getSource().getTrueSource();
+
+            // 检测 flame_sword 攻击
+            ItemStack heldItem = attacker.getHeldItemMainhand();
+            if (heldItem.getItem() == ItemsRegister.flame_sword) {
+                // 获取被攻击的uuid
+                Entity target = event.getEntity();
+
+                // 获取记分板
+                Scoreboard scoreboard = attacker.getEntityWorld().getScoreboard();
+                String objectiveName = "flame_attacks";
+                ScoreObjective objective = scoreboard.getObjective(objectiveName);
+
+                if (objective == null) {
+                    // 如果目标不存在，创建一个新的记分板目标
+                    objective = scoreboard.addScoreObjective(objectiveName, net.minecraft.scoreboard.ScoreCriteria.DUMMY);
+                    objective.setDisplayName("Flame Sword Hits");
+                    scoreboard.setObjectiveInDisplaySlot(1, objective); // 右侧显示
+                }
+
+                // 用uuid记名
+                String targetUUID = target.getUniqueID().toString();
+                Score score = scoreboard.getOrCreateScore(targetUUID, objective);
+
+                // 更新分数
+                score.setScorePoints(score.getScorePoints() + 1);
             }
         }
-
     }
 }
