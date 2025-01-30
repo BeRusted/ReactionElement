a3239f1670c97b03a2cc355f20a84c886ef135e4
lewe189 <lewe@mplan.site>
2025-01-30 13:04:36 +0800

    反应仓库加入占位函数

--- src/main/java/com/BeRusted/ReactionElement/element/reactionDepot.java
+++ src/main/java/com/BeRusted/ReactionElement/element/reactionDepot.java
@@ -2,41 +2,147 @@ package com.BeRusted.ReactionElement.element;
 
 public class reactionDepot {
     // 同元素反应 //
+
     // 火火
+    public static void fireAndFire() {
+        System.out.println("发生火火同元素反应");
+    }
+
     // 水水
+    public static void waterAndWater() {
+        System.out.println("发生水水同元素反应");
+    }
+
     // 冰冰
+    public static void iceAndIce() {
+        System.out.println("发生冰冰同元素反应");
+    }
+
     // 生命生命
+    public static void healthAndHealth() {
+        System.out.println("发生生命生命同元素反应");
+    }
+
     // 电电
+    public static void lightningAndLightning() {
+        System.out.println("发生电电同元素反应");
+    }
+
     // 光光
+    public static void lightAndLight() {
+        System.out.println("发生光光同元素反应");
+    }
+
     // 暗暗
+    public static void darkAndDark() {
+        System.out.println("发生暗暗同元素反应");
+    }
 
     // 不同元素反应 //
 
     // 火水
+    public static void fireAndWater() {
+        System.out.println("发生火水不同元素反应");
+    }
+
     // 火冰
+    public static void fireAndIce() {
+        System.out.println("发生火冰不同元素反应");
+    }
+
     // 火生命
+    public static void fireAndHealth() {
+        System.out.println("发生火生命不同元素反应");
+    }
+
     // 火电
+    public static void fireAndLightning() {
+        System.out.println("发生火电不同元素反应");
+    }
+
     // 火光
+    public static void fireAndLight() {
+        System.out.println("发生火光不同元素反应");
+    }
+
     // 火暗
+    public static void fireAndDark() {
+        System.out.println("发生火暗不同元素反应");
+    }
 
     // 水冰
+    public static void waterAndIce() {
+        System.out.println("发生水冰不同元素反应");
+    }
+
     // 水生命
+    public static void waterAndHealth() {
+        System.out.println("发生水生命不同元素反应");
+    }
+
     // 水电
+    public static void waterAndLightning() {
+        System.out.println("发生水电不同元素反应");
+    }
+
     // 水光
+    public static void waterAndLight() {
+        System.out.println("发生水光不同元素反应");
+    }
+
     // 水暗
+    public static void waterAndDark() {
+        System.out.println("发生水暗不同元素反应");
+    }
 
     // 冰生命
+    public static void iceAndHealth() {
+        System.out.println("发生冰生命不同元素反应");
+    }
+
     // 冰电
+    public static void iceAndLightning() {
+        System.out.println("发生冰电不同元素反应");
+    }
+
     // 冰光
+    public static void iceAndLight() {
+        System.out.println("发生冰光不同元素反应");
+    }
+
     // 冰暗
+    public static void iceAndDark() {
+        System.out.println("发生冰暗不同元素反应");
+    }
 
     // 生命电
+    public static void healthAndLightning() {
+        System.out.println("发生生命电不同元素反应");
+    }
+
     // 生命光
+    public static void healthAndLight() {
+        System.out.println("发生生命光不同元素反应");
+    }
+
     // 生命暗
+    public static void healthAndDark() {
+        System.out.println("发生生命暗不同元素反应");
+    }
 
     // 电光
+    public static void lightningAndLight() {
+        System.out.println("发生电光不同元素反应");
+    }
+
     // 电暗
+    public static void lightningAndDark() {
+        System.out.println("发生电暗不同元素反应");
+    }
 
     // 光暗
+    public static void lightAndDark() {
+        System.out.println("发生光暗不同元素反应");
+    }
 
 }
